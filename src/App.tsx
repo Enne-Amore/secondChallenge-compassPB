c05d1c0b57f0858ddf89e9d6215a737975ea1773
Enne Jim Pessoa <ennefofe10@gmail.com>
2024-11-04 16:50:17 -0300

    feat: router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { HomePage } from "./pages/HomePage"
+import { Login } from "./pages/Login"
+
 export default function App() {
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage/>}/>
+          <Route path="/resultado" element={<Login/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
