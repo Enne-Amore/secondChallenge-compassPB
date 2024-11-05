1d6384dcda3d7aee2596af0e2f4a2a8d22370fd7
Gabriel Costa de Jesus <gabrielcostadeveloper@gmail.com>
2024-11-05 15:47:11 -0300

    feat: add header folder structure

---
+++ src/components/header/Navbar.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Navbar: React.FC = () => {
+  return (
+    <nav className="w-full md:w-auto flex flex-col md:flex-row items-center space-y-2 md:space-y-0 md:space-x-10 mt-4 md:mt-0">
+      <ul className="flex flex-col md:flex-row items-center space-y-2 md:space-y-0 md:space-x-10">
+        <li><a href="#" className="text-white text-sm md:text-lg">Home</a></li>
+        <li><a href="#" className="text-white text-sm md:text-lg">About</a></li>
+        <li><a href="#" className="text-white text-sm md:text-lg">Settings</a></li>
+        <li><a href="#" className="text-white text-sm md:text-lg">Profile</a></li>
+      </ul>
+
+      {/* Campo de Busca */}
+      <input 
+        type="search" 
+        placeholder="Search in site" 
+        className="hidden md:block ml-4 bg-transparent border border-white/50 rounded-md max-w-xs h-9 px-2 text-white placeholder-white bg-no-repeat bg-right"
+      />
+    </nav>
+  );
+};
+
+export default Navbar;
