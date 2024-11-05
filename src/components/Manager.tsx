65d5f7d80953048322cc76f7e1b8f3f61fcd20d1
Orlean Sousa <99688852+OrleanSousa@users.noreply.github.com>
2024-11-05 11:36:11 -0300

    feat: addition of the second session and finalization of the main text styles.

--- src/components/Manager.tsx
+++ src/components/Manager.tsx
@@ -1,8 +1,29 @@
 
+import styles from './Manager.module.css'
 
 const Manager = () => {
   return (
-    <div>manager</div>
+    
+    <section className={styles.manager}> 
+    <div className={styles.container}>
+      <h2 className={styles.title}>The manager</h2>
+      <p className={styles.text}>
+      Project Manager is a tool designed to make organizing and tracking your daily 
+      tasks easier than ever. With a clean, intuitive interface and powerful features, 
+      you can plan, prioritize, and keep tabs on your projects - whether you’re 
+      working solo or a with a team
+      </p>
+      <button className={styles.learButton}>Learn More</button>
+    </div>
+      <div className={styles.containList}>
+        <div>
+          <img src="../assets/track-tasks-illustration.png" alt="Imagem 1" className="w-full h-48 object-cover rounded" />
+          <h2>Manage Projects</h2>
+          <p>20+ projects</p>
+        </div>
+      
+    </div>
+    </section>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,24 +9,31 @@ export default {
       colors: {
         'black-55': 'rgba(0, 0, 0, 0.55)',
         'blue-55' : '#92BBFF',
-        "bronw-70" : 'rgba(35, 8, 28, 0.7)' 
+        "bronw-70" : 'rgba(35, 8, 28, 0.7)' ,
+        'blue-750' : '#1E293B',
+        'yellow-150' : '#FCF4B6',
+        'blue-850' : '#160A60'
       },
       width: {
         '342': '342px',
+        '392' : '392px'
       },
       height: {
         '285': '285px',
+        '794' : '794px'
     },
       fontSize: {
         '24px': '24px',
   },
-  fontFamily: {
+    fontFamily: {
     roboto: ['Roboto', 'sans-serif'],
   },
-  margin: {
+    margin: {
     'margin-b': '39px', 
-  
-  },
+    },
+    padding: {
+      'person1' : '60px'
+    },
   plugins: [],
   }
   }
