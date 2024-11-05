f403f9f77ca1c45ff5cb4c08907858b94ad58bce
Erica Farias <ericafarias092@gmail.com>
2024-11-05 12:21:31 -0300

    feat: adding the skeleton of the sing in page.

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,8 +1,50 @@
 export const SignIn = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
-  
+    return (
+        <div>
+            <div>
+                <h1>Sing up Information</h1>
+                <p>
+                    Already have an account? <a>Log in</a>
+                </p>
+            </div>
+            <div>
+                <div>
+                    <label>First name</label>
+                    <input type="text" placeholder="Enter your first name" />
+                </div>
+                <div>
+                    <label>Last name</label>
+                    <input type="text" placeholder="Enter your last name" />
+                </div>
+            </div>
+            <div>
+                <div>
+                    <div>
+                        <label>Email</label>
+                        <input type="text" placeholder="Enter your email" />
+                    </div>
+                    <div>
+                        <label>Job position</label>
+                        <input
+                            type="text"
+                            placeholder="Enter your job position (example:Project Manager)"
+                        />
+                    </div>
+                    <div>
+                        <label>Password</label>
+                        <input
+                            type="password"
+                            placeholder="Enter your password"
+                        />
+                    </div>
+                </div>
+            </div>
+            <button />
+            <small>or sing in with...</small>
+            <div>
+                <button>facebook</button>
+                <button>gmail</button>
+            </div>
+        </div>
+    );
+};
