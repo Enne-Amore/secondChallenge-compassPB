550b51f97d08c597163e6e9abd2debf071b24f4e
Orlean Sousa <99688852+OrleanSousa@users.noreply.github.com>
2024-11-05 12:19:10 -0300

    feat: completion of session 2 by assigning side scrolling to the images

--- src/components/Manager.tsx
+++ src/components/Manager.tsx
@@ -1,10 +1,13 @@
-
+import project from '../assets/project-manager-illustration.png'
+import tasks from '../assets/track-tasks-illustration.png'
+import collaborate from '../assets/collaborate-illustration.png'
 import styles from './Manager.module.css'
 
 const Manager = () => {
   return (
     
     <section className={styles.manager}> 
+
     <div className={styles.container}>
       <h2 className={styles.title}>The manager</h2>
       <p className={styles.text}>
@@ -16,11 +19,34 @@ const Manager = () => {
       <button className={styles.learButton}>Learn More</button>
     </div>
       <div className={styles.containList}>
-        <div>
-          <img src="../assets/track-tasks-illustration.png" alt="Imagem 1" className="w-full h-48 object-cover rounded" />
-          <h2>Manage Projects</h2>
-          <p>20+ projects</p>
+        <div className={styles.card}>
+          <div className={styles.containImg}>
+            <img src={project} alt="Imagem 1" className={styles.img}/>
+            <div className={styles.containtext}>
+              <h2>Manage Projects</h2>
+              <p className={styles.textcard}>20+ projects</p>
+            </div>
+          </div>
         </div>
+        <div className={styles.card}>
+          <div className={styles.containImg}>
+            <img src={tasks} alt="Imagem 1" className={styles.img}/>
+            <div className={styles.containtext}>
+              <h2>Track Tasks</h2>
+              <p className={styles.textcard}>50+ tasks</p>
+            </div>
+          </div>
+        </div>
+        <div className={styles.card}>
+          <div className={styles.containImg}>
+            <img src={collaborate} alt="Imagem 1" className={styles.img}/>
+            <div className={styles.containtext}>
+              <h2>Manage Projects</h2>
+              <p className={styles.textcard}>20+ projects</p>
+            </div>
+          </div>
+        </div>
+        
       
     </div>
     </section>
