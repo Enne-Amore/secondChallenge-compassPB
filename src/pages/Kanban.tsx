1cc8028466e798b73773ef9732d6b1e91b0f2212
Enne Jim Pessoa <ennefofe10@gmail.com>
2024-11-05 20:31:42 -0300

    feat: aside

--- src/pages/Kanban.tsx
+++ src/pages/Kanban.tsx
@@ -1,12 +1,13 @@
 import { TbPinnedFilled } from "react-icons/tb"
 import { PersonProfile } from "../components/PersonProfile"
 import { Task } from "../components/Task"
+import logo from "../../public/logo.png"
 
 export const Kanban = () => {
   return (
     <main className="flex flex-col items-center">
       <div className="relative text-white w-max h-max">
-        <TbPinnedFilled className="absolute top-5 right-1 bg-[#94A3B8] text-xl w-max h-max rounded-full p-3" />
+        <TbPinnedFilled className="absolute top-5 right-1 bg-[#94A3B8] hover:bg-[#1E293B] transition-all cursor-pointer text-xl w-max h-max rounded-full p-3" />
 
         <div className="bg-[#6C7D96] rounded-3xl px-5 py-8 font-bold flex-col gap-6 hidden">
           <section>
@@ -141,6 +142,24 @@ export const Kanban = () => {
           </ul>
         </article>
       </section>
+
+      <aside className="bg-[#00000080] text-white rounded-2xl flex flex-col gap-2 pt-1.5 pb-3 px-2.5 -translate-y-full w-10/12">
+        <div className="flex items-center gap-2">
+          <figure className="w-6 h-6">
+            <img src={logo} alt="Project Manager logo" className="w-full h-full rounded-full" />
+          </figure>
+
+          <h2 className="text-base font-bold">Trabalhar no aplicativo</h2>
+        </div>
+
+        <p className="text-sm font-normal">Adicione ideias, notas e faça um plano de organização de onde estiver.</p>
+
+        <div className="flex justify-between items-center mt-1">
+          <button type="button" className="bg-[#FFFCFCEB] text-black text-sm font-medium rounded-2xl p-4 py-1">Agora não</button>
+
+          <a href="" className="bg-[#4F46E5] text-sm font-medium rounded-2xl p-4 py-1">Usar o aplicativo</a>
+        </div>
+      </aside>
     </main>
   )
 }
