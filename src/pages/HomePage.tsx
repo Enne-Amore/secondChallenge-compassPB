99c8dd6c9ff78693e6312e7404f8f684c90cdf1b
Orlean Sousa <99688852+OrleanSousa@users.noreply.github.com>
2024-11-06 09:05:47 -0300

    feat:completion of the subscribe area and beginning of the creation of the JSON file

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,7 @@
 import Feature from "../components/home/Feature"
 import Main from "../components/home/Main"
 import Manager from "../components/home/Manager"
+import Subscribe from "../components/home/Subscribe"
 
 
 
@@ -11,6 +12,7 @@ export const HomePage = () => {
         <Main/>
         <Manager/>
         <Feature/>
+        <Subscribe/>
     </>
   )
 }
