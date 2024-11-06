3c181969c4159ce115126faaf2b8536b86aea9b0
Enne Jim Pessoa <ennefofe10@gmail.com>
2024-11-05 21:05:14 -0300

    style: TableTask component

--- src/pages/Kanban.tsx
+++ src/pages/Kanban.tsx
@@ -1,7 +1,7 @@
 import { TbPinnedFilled } from "react-icons/tb"
 import { PersonProfile } from "../components/PersonProfile"
-import { Task } from "../components/Task"
 import logo from "../../public/logo.png"
+import { TableTask } from "../components/TableTask"
 
 export const Kanban = () => {
   return (
@@ -76,71 +76,11 @@ export const Kanban = () => {
       </div>
 
       <section className="flex gap-4 bg-kanban bg-no-repeat bg-cover rounded-3xl py-60 w-10/12 overflow-auto">
-        <article className="bg-[#1E293B1A] rounded-3xl">
-          <div className="bg-[#D3D0F8] flex items-center justify-between py-1 pl-2 pr-3 rounded-full">
-            <div className="flex items-center gap-2.5">
-              <strong className="bg-[#4F46E5] text-white text-xs pt-1.5 pb-2.5 px-3 rounded-3xl font-semibold">25</strong>
-              
-              <span className="text-[#4F46E5] text-xs font-bold">To do</span>
-            </div>
+        <TableTask title="To do" qtd={25} darkColor="#4F46E5" lightColor="#D3D0F8" />
 
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 9 10" fill="none">
-              <path d="M7.96113 4.91918C7.96113 5.02419 7.91941 5.12491 7.84515 5.19916C7.7709 5.27342 7.67018 5.31514 7.56517 5.31514H5.05743V7.82287C5.05743 7.92789 5.01571 8.0286 4.94146 8.10286C4.8672 8.17712 4.76649 8.21883 4.66147 8.21883C4.55646 8.21883 4.45574 8.17712 4.38149 8.10286C4.30723 8.0286 4.26551 7.92789 4.26551 7.82287V5.31514H1.75778C1.65276 5.31514 1.55205 5.27342 1.47779 5.19916C1.40353 5.12491 1.36182 5.02419 1.36182 4.91918C1.36182 4.81416 1.40353 4.71345 1.47779 4.63919C1.55205 4.56494 1.65276 4.52322 1.75778 4.52322H4.26551V2.01548C4.26551 1.91047 4.30723 1.80975 4.38149 1.7355C4.45574 1.66124 4.55646 1.61952 4.66147 1.61952C4.76649 1.61952 4.8672 1.66124 4.94146 1.7355C5.01571 1.80975 5.05743 1.91047 5.05743 2.01548V4.52322H7.56517C7.67018 4.52322 7.7709 4.56494 7.84515 4.63919C7.91941 4.71345 7.96113 4.81416 7.96113 4.91918Z" fill="#4F46E5"/>
-            </svg>
-          </div>
-
-          <ul className="flex flex-col gap-3.5 py-3 px-3.5">
-            <Task />
-            
-            <Task />
-            
-            <Task />
-          </ul>
-        </article>
-
-        <article className="bg-[#1E293B1A] rounded-3xl">
-          <div className="bg-[#FCE6C2] flex items-center justify-between py-1 pl-2 pr-3 rounded-full">
-            <div className="flex items-center gap-2.5">
-              <strong className="bg-[#F59E0B] text-white text-xs pt-1.5 pb-2.5 px-3 rounded-3xl font-semibold">8</strong>
-              
-              <span className="text-[#F59E0B] text-xs font-bold">In progress</span>
-            </div>
-
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 9 10" fill="none">
-              <path d="M7.96113 4.91918C7.96113 5.02419 7.91941 5.12491 7.84515 5.19916C7.7709 5.27342 7.67018 5.31514 7.56517 5.31514H5.05743V7.82287C5.05743 7.92789 5.01571 8.0286 4.94146 8.10286C4.8672 8.17712 4.76649 8.21883 4.66147 8.21883C4.55646 8.21883 4.45574 8.17712 4.38149 8.10286C4.30723 8.0286 4.26551 7.92789 4.26551 7.82287V5.31514H1.75778C1.65276 5.31514 1.55205 5.27342 1.47779 5.19916C1.40353 5.12491 1.36182 5.02419 1.36182 4.91918C1.36182 4.81416 1.40353 4.71345 1.47779 4.63919C1.55205 4.56494 1.65276 4.52322 1.75778 4.52322H4.26551V2.01548C4.26551 1.91047 4.30723 1.80975 4.38149 1.7355C4.45574 1.66124 4.55646 1.61952 4.66147 1.61952C4.76649 1.61952 4.8672 1.66124 4.94146 1.7355C5.01571 1.80975 5.05743 1.91047 5.05743 2.01548V4.52322H7.56517C7.67018 4.52322 7.7709 4.56494 7.84515 4.63919C7.91941 4.71345 7.96113 4.81416 7.96113 4.91918Z" fill="#F59E0B"/>
-            </svg>
-          </div>
-
-          <ul className="flex flex-col gap-3.5 py-3 px-3.5">
-            <Task />
-            
-            <Task />
-            
-            <Task />
-          </ul>
-        </article>
-
-        <article className="bg-[#1E293B1A] rounded-3xl">
-          <div className="bg-[#C7F0D6] flex items-center justify-between py-1 pl-2 pr-3 rounded-full">
-            <div className="flex items-center gap-2.5">
-              <strong className="bg-[#22C55E] text-white text-xs pt-1.5 pb-2.5 px-3 rounded-3xl font-semibold">2</strong>
-              
-              <span className="text-[#22C55E] text-xs font-bold">Done</span>
-            </div>
-
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 9 10" fill="none">
-              <path d="M7.96113 4.91918C7.96113 5.02419 7.91941 5.12491 7.84515 5.19916C7.7709 5.27342 7.67018 5.31514 7.56517 5.31514H5.05743V7.82287C5.05743 7.92789 5.01571 8.0286 4.94146 8.10286C4.8672 8.17712 4.76649 8.21883 4.66147 8.21883C4.55646 8.21883 4.45574 8.17712 4.38149 8.10286C4.30723 8.0286 4.26551 7.92789 4.26551 7.82287V5.31514H1.75778C1.65276 5.31514 1.55205 5.27342 1.47779 5.19916C1.40353 5.12491 1.36182 5.02419 1.36182 4.91918C1.36182 4.81416 1.40353 4.71345 1.47779 4.63919C1.55205 4.56494 1.65276 4.52322 1.75778 4.52322H4.26551V2.01548C4.26551 1.91047 4.30723 1.80975 4.38149 1.7355C4.45574 1.66124 4.55646 1.61952 4.66147 1.61952C4.76649 1.61952 4.8672 1.66124 4.94146 1.7355C5.01571 1.80975 5.05743 1.91047 5.05743 2.01548V4.52322H7.56517C7.67018 4.52322 7.7709 4.56494 7.84515 4.63919C7.91941 4.71345 7.96113 4.81416 7.96113 4.91918Z" fill="#22C55E"/>
-            </svg>
-          </div>
-
-          <ul className="flex flex-col gap-3.5 py-3 px-3.5">
-            <Task />
-            
-            <Task />
-            
-            <Task />
-          </ul>
-        </article>
+        <TableTask title="In progress" qtd={8} darkColor="#F59E0B" lightColor="#FCE6C2" />
+        
+        <TableTask title="Done" qtd={5} darkColor="#22C55E" lightColor="#C7F0D6" />
       </section>
 
       <aside className="bg-[#00000080] text-white rounded-2xl flex flex-col gap-2 pt-1.5 pb-3 px-2.5 -translate-y-full w-10/12">
