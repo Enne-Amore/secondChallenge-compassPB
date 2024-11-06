e0c1a22a11d8ddd9967ec6582e9061620eb12e4b
Erica Farias <ericafarias092@gmail.com>
2024-11-06 18:45:28 -0300

    refactor: changing the structure.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { HomePage } from "./pages/HomePage"
-import { Login } from "./pages/Login"
-import './index.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HomePage } from "./pages/HomePage";
+import { Login } from "./pages/Login";
+import "./index.css";
+import { SignIn } from "./pages/SingIn/SignIn";
 
 export default function App() {
-  return (
-    <>
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
 
-    
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage/>}/>
-
-          <Route path="/login" element={<Login/>}/>
-        </Routes>
-      </BrowserRouter>
-    </>
-  )
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/singIn" element={<SignIn />} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
