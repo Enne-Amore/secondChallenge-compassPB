35ba3b7d858ae24be85a3c88b0a0f69187b0516c
Gabriel Costa de Jesus <gabrielcostadeveloper@gmail.com>
2024-11-06 16:49:23 -0300

    feat: mobile footer finalized

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -7,6 +7,11 @@ const Footer = () => {
     <footer className={styles.footer}>
         <div className={styles.container}>
             <IconsSocials/>
+            <ul className={styles.ul}>
+                <li className={styles.li}>@ 2024 Project Manager. All rights reserved.</li>
+                <li className={styles.li}><a href="https://policies.google.com/privacy?hl=en-US" target="_blank">Privacy policy</a></li>
+                <li className={styles.li}><a href="https://policies.google.com/terms?hl=en-US" target="_blank">Terms of Service</a></li>
+            </ul>
         </div>
     </footer>
   )
