f528cdcffe5b35663fb3ec4dbce059f9dfdc12dd
Enne Jim Pessoa <ennefofe10@gmail.com>
2024-11-06 22:21:03 -0300

    style: tables tasks component

--- src/pages/Kanban.tsx
+++ src/pages/Kanban.tsx
@@ -1,34 +1,13 @@
-import { TableTask } from "../components/kanban/TableTask";
 import { FixedCard } from "../components/kanban/FixedCard";
 import { Notification } from "../components/kanban/Notification";
+import { TablesTasks } from "../components/kanban/TablesTasks";
 
 export const Kanban = () => {
   return (
     <main className="relative flex flex-col items-center">
       <FixedCard />
 
-      <section className="flex gap-4 bg-kanban bg-no-repeat bg-cover rounded-3xl py-60 w-11/12 overflow-auto">
-        <TableTask
-          title={"To do"}
-          qtd={25}
-          darkColor={"#4F46E5"}
-          lightColor={"#D3D0F8"}
-        />
-
-        <TableTask
-          title={"In progress"}
-          qtd={8}
-          darkColor={"#F59E0B"}
-          lightColor={"#FCE6C2"}
-        />
-
-        <TableTask
-          title={"Done"}
-          qtd={5}
-          darkColor={"#22C55E"}
-          lightColor={"#C7F0D6"}
-        />
-      </section>
+      <TablesTasks />
 
       <Notification />
     </main>
