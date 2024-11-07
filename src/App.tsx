8aaf5d8c472377e8a856365d9ed43676ae05dfb3
Erica Farias <ericafarias092@gmail.com>
2024-11-06 22:35:51 -0300

    feat: adding sign in route

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
-import { Login } from "./pages/Login";
+
 import "./index.css";
-import { SignIn } from "./pages/SingIn/SignIn";
 import { Kanban } from "./pages/Kanban";
+import { LoginPage } from "./pages/LoginPage";
+import { SigninPage } from "./pages/SigninPage";
 
 export default function App() {
     return (
@@ -12,9 +13,9 @@ export default function App() {
                 <Routes>
                     <Route path="/" element={<HomePage />} />
 
-                    <Route path="/login" element={<Login />} />
+                    <Route path="/login" element={<LoginPage />} />
 
-                    <Route path="/signIn" element={<SignIn />} />
+                    <Route path="/signin" element={<SigninPage />} />
 
                     <Route path="/kanban" element={<Kanban />} />
                 </Routes>
