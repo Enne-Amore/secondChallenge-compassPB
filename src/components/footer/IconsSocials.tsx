821bfbf8c51b4fb59826243766b5dc2e250f1214
Gabriel Costa de Jesus <gabrielcostadeveloper@gmail.com>
2024-11-07 15:00:32 -0300

    feat: add link instagram

--- src/components/footer/IconsSocials.tsx
+++ src/components/footer/IconsSocials.tsx
@@ -8,7 +8,7 @@ const IconsSocials = () => {
       <a href="https://facebook.com" target="_blank" className={styles.icon}>
           <FaFacebookF className={`${styles.iconSVG} ${styles.iconFacebook}`}/>
       </a>
-      <a href="https://instagram.com" target="_blank" className={styles.icon}>
+      <a href="https://www.instagram.com/manager.projectg2/" target="_blank" className={styles.icon}>
           <FaInstagram className={`${styles.iconSVG} ${styles.iconInstagram}`}/>
       </a>
       <a href="https://linkedin.com" target="_blank" className={styles.icon}>
