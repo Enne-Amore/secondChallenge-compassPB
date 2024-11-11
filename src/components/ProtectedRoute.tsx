380b44f5734fcda71c29cb0e22a428dc63aa7af7
Antonio Santiago <santiagojrbv@gmail.com>
2024-11-11 15:51:58 -0300

    Feature:Adjust Loading
    Adjusting loading screen to look prettier

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,30 +1,31 @@
-import React, { ReactNode } from "react";
+// ProtectedRoute.js
+import React from "react";
 import { Navigate } from "react-router-dom";
 import { useUser } from "@clerk/clerk-react";
 
-interface ProtectedRouteProps {
-  children: ReactNode;
-}
-
-const Loading: React.FC = () => (
-  <div className="flex items-center justify-center h-screen">
+// Componente de Loading para exibir durante o carregamento
+const Loading = () => (
+    <div className="flex items-center justify-center h-screen">
     <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-primary"></div>
-    <p className="ml-4 text-lg font-medium text-gray-700">Loading...</p>
+    <p className="ml-4 text-lg font-medium text-gray-700">L...</p>
   </div>
 );
 
-const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
-  const { isSignedIn, isLoaded } = useUser();
+function ProtectedRoute({ children }) {
+    const { isSignedIn, isLoaded } = useUser();
 
-  if (!isLoaded) {
-    return <Loading />;
-  }
+    // Se o estado de autenticação ainda estiver carregando, mostra a tela de carregamento
+    if (!isLoaded) {
+        return <Loading />;
+    }
 
-  if (!isSignedIn) {
-    return <Navigate to="/403" replace />;
-  }
+    // Se o usuário não está autenticado, redireciona para a página 403
+    if (!isSignedIn) {
+        return <Navigate to="/403" replace />;
+    }
 
-  return <>{children}</>;
-};
+    // Caso esteja autenticado e o estado carregado, renderiza o componente filho
+    return children;
+}
 
 export default ProtectedRoute;
